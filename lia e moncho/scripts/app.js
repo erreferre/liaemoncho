5feead7416e668f044ae2ce56880fad5209892e1
ramon fernandez <ramon.fernandez@fractalia.es>
2014-10-08 22:26:58 +0000

    Changes made by ramon fernandez (ramon.fernandez@fractalia.es).

--- lia e moncho/scripts/app.js
+++ lia e moncho/scripts/app.js
@@ -8,14 +8,14 @@
     // create an object to store the models for each view
     window.APP = {
       models: {
-        home: {
-          title: 'Home'
+        camara: {
+          title: 'Camara'
         },
-        settings: {
-          title: 'Settings'
+        axenda: {
+          title: 'Axenda do Casamento'
         },
-        contacts: {
-          title: 'Contacts',
+        fotos: {
+          title: 'Fotos',
           ds: new kendo.data.DataSource({
             data: [{ id: 1, name: 'Bob' }, { id: 2, name: 'Mary' }, { id: 3, name: 'John' }]
           }),
@@ -33,6 +33,9 @@
       // Cordova will wait 5 very long seconds to do it for you.
       navigator.splashscreen.hide();
 
+      //document.addEventListener("menubutton", exitAppPopup, false);
+      document.addEventListener("backbutton", exitAppPopup, false);
+
       app = new kendo.mobile.Application(document.body, {
         
         // you can change the default transition (slide, zoom or fade)
@@ -43,10 +46,26 @@
         skin: 'flat',
 
         // the application needs to know which view to load first
-        initial: 'views/home.html'
+        initial: 'views/axenda.html'
       });
 
     }, false);
 
-
+    //opcion de Exit
+    function exitAppPopup() {
+        navigator.notification.confirm(
+            "pásanos o teu mail a erreferre@gmail.com ou liacarballal@msn.com, e mandarémosche as fotos"
+            , function(button) {
+                  if (button === 2) {
+                      //window.plugins.powerManagement.release();
+                      //stopSelfie();
+                      navigator.app.exitApp();
+                  } 
+              }
+            , "¿Sair da App?"
+            , "Pois non, Pois si"
+        );
+        return false;
+    };
+    
 }());
