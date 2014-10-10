6a91b0a1c576ba6efb336e4a0ca292e574228ed9
liaemoncho liaemoncho <liaemoncho@aerowi.es>
2014-10-10 00:42:06 +0000

    Changes made by liaemoncho liaemoncho (liaemoncho@aerowi.es).

--- lia e moncho/scripts/app.js
+++ lia e moncho/scripts/app.js
@@ -1,4 +1,3 @@
-
 (function () {
 
     // store a reference to the application object that will be created
@@ -7,65 +6,56 @@
 
     // create an object to store the models for each view
     window.APP = {
-      models: {
-        camara: {
-          title: 'Camara'
-        },
-        axenda: {
-          title: 'Axenda do Casamento'
-        },
-        fotos: {
-          title: 'Fotos',
-          ds: new kendo.data.DataSource({
-            data: [{ id: 1, name: 'Bob' }, { id: 2, name: 'Mary' }, { id: 3, name: 'John' }]
-          }),
-          alert: function(e) {
-            alert(e.data.name);
-          }
+        models: {
+            camara: {
+                title: 'Camara'
+            },
+            axenda: {
+                title: 'Axenda do Casamento'
+            },
+            fotos: {
+                title: 'Fotos'
+            }
         }
-      }
     };
 
     // this function is called by Cordova when the application is loaded by the device
-    document.addEventListener('deviceready', function () {  
-      
-      // hide the splash screen as soon as the app is ready. otherwise
-      // Cordova will wait 5 very long seconds to do it for you.
-      navigator.splashscreen.hide();
+    document.addEventListener('deviceready', function () {
 
-      //document.addEventListener("menubutton", exitAppPopup, false);
-      document.addEventListener("backbutton", exitAppPopup, false);
+        // hide the splash screen as soon as the app is ready. otherwise
+        // Cordova will wait 5 very long seconds to do it for you.
+        navigator.splashscreen.hide();
 
-      app = new kendo.mobile.Application(document.body, {
-        
-        // you can change the default transition (slide, zoom or fade)
-        transition: 'slide',
-        
-        // comment out the following line to get a UI which matches the look
-        // and feel of the operating system
-        skin: 'flat',
+        document.addEventListener("menubutton", exitAppPopup, false);
+        //document.addEventListener("backbutton", exitAppPopup, false);
 
-        // the application needs to know which view to load first
-        initial: 'views/axenda.html'
-      });
+        app = new kendo.mobile.Application(document.body, {
+
+            // you can change the default transition (slide, zoom or fade)
+            transition: 'slide',
+
+            // comment out the following line to get a UI which matches the look
+            // and feel of the operating system
+            skin: 'flat',
+
+            // the application needs to know which view to load first
+            initial: 'views/axenda.html'
+        });
 
     }, false);
 
     //opcion de Exit
     function exitAppPopup() {
         navigator.notification.confirm(
-            "pásanos o teu mail a erreferre@gmail.com ou liacarballal@msn.com, e mandarémosche as fotos"
-            , function(button) {
-                  if (button === 2) {
-                      //window.plugins.powerManagement.release();
-                      //stopSelfie();
-                      navigator.app.exitApp();
-                  } 
-              }
-            , "¿Sair da App?"
-            , "Pois non, Pois si"
+            "pásanos o teu mail a erreferre@gmail.com ou liacarballal@msn.com, e mandarémosche as fotos", function (button) {
+                if (button === 2) {
+                    //window.plugins.powerManagement.release();
+                    //stopSelfie();
+                    navigator.app.exitApp();
+                }
+            }, "¿Sair da App?", "Pois non, Pois si"
         );
         return false;
     };
-    
+
 }());
